c793f0721945f57356a2cbe4e64a3a2cd44abc34
Lauro Caetano <laurocaetano1@gmail.com>
2022-10-24 23:16:16 +0200

    Adjust the storage interface
    And document its expected behaviour

--- storage/storage.go
+++ storage/storage.go
@@ -5,8 +5,24 @@
 // `disk`, `remote` and etc.
 package storage
 
+// High level interface, that enables backend
+// implementation for data storage.
 type Storage interface {
-	Put(key string, value byte) error
-	Get(key string) (byte, error)
+
+	// Add a new key/value pair to the storage
+	// `key` is a string, that has no limit at the moment.
+	//
+	// `value` is a `byte` array that is not limited
+	// at the moment.
+	Put(key string, value []byte) error
+
+	// Gets the value assigned to a given `key`. In
+	// case the `key` does not exist, it will return
+	// an error.
+	Get(key string) ([]byte, error)
+
+	// Deletes and existing key and its associated
+	// value. In case the given `key` does not exist,
+	// an error is returned instead.
 	Delete(key string) error
 }
